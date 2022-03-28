8f955cb8c8dcde04c49389f09dfb16acfccfb875
hjjo <compa513@naver.com>
2022-03-28 23:00:39 +0900

    first commit

---
+++ chapter1.js
@@ -0,0 +1,27 @@
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    // 포인트를 적립한다
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역을 출력한다.
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += amountFor(perf);
+  }
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
