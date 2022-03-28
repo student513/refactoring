7f34e9005803abe215c60748a32539c56b313bde
hjjo <compa513@naver.com>
2022-03-28 23:40:57 +0900

    반복문으로부터 함수 추출하기

--- chapter1.js
+++ chapter1.js
@@ -3,24 +3,30 @@ function statement(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount() / 100)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
 
-function totalVolumeCredits() {
-  let volumeCredits = 0;
+function totalAmount() {
+  let result = 0;
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
+    result += amountFor(perf);
   }
-  return volumeCredits;
+  return result;
+}
+
+function totalVolumeCredits() {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += volumeCreditsFor(perf);
+  }
+  return result;
 }
 
 function usd(aNumber) {
