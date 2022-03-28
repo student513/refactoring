22dd4bf1fe9ecf80f1651e937040719b4837af6f
hjjo <compa513@naver.com>
2022-03-28 23:46:16 +0900

    빠진 함수 추가

--- chapter1.js
+++ chapter1.js
@@ -44,3 +44,29 @@ function volumeCreditsFor(aPerformance) {
     volumeCredits += Math.floor(aPerformance.audience / 5);
   return volumeCredits;
 }
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
+
+function amountFor(aPerformance) {
+  let result = 0;
+  switch (playFor(aPerformance).type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+  }
+  return result;
+}
