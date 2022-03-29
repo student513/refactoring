df78f88c7a5d1742fccdeefb229cc25aef4eb611
hjjo <compa513@naver.com>
2022-03-29 09:48:30 +0900

    별도함수 분리

--- chapter1.js
+++ chapter1.js
@@ -1,10 +1,14 @@
 function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
+  return renderPlainText(createStatementData(invoice, plays));
+
+  function createStatementData(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+  }
 
   function totalAmount() {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
